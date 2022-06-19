cd7c620dc0927f58078274c6e285ca26e3d167e0
Unicornatech <65677532+Unicornatech@users.noreply.github.com>
2022-06-19 16:14:14 -0400

    Add files via upload

---
+++ Reset.bat
@@ -0,0 +1,12 @@
+@EchoOFF
+
+Echo enter Y to start the Reset
+rmdir /s OSX
+Echo Resetting CrenalOSX This might take awhile
+gh repo clone OSX
+Echo Updates installed successfuly!
+Echo We recommend you move the backup folder
+pause
+Echo Rebooting the system
+cd C:\CrenalOS\Versions\OSX
+CrenalOSX.bat
